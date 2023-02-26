2f4729708ca7cd1b50bbaaf4674878ced1c4e9f9
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-02-25 22:14:28 -0300

    Organizando uma pasta para exercícios e arquivo para aula.

---
+++ PythonExercicios/ex001.py
@@ -0,0 +1,5 @@
+# Crie um programa que escreva "Olá, mundo!" na tela.
+
+msg = "Olá, mundo!"
+print(msg)
+
--- PythonExercicios/ex002.py
+++ PythonExercicios/ex002.py
@@ -1,2 +1,2 @@
-nome = input("Qual é o seu nome?\n")
+nome = input("Digite o seu nome?\n")
 print(f"É um grande prazer te conhecer, {nome}.")
