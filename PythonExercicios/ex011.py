9185d9c9b59249d8a9c01af215138b735456cefd
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-02-27 19:51:53 -0300

    Exercícios a fazer.

---
+++ PythonExercicios/ex011.py
@@ -0,0 +1,2 @@
+#Faça um programa que leia a largura e a altura de uma parede em metros, calcule sua área e a quantidade de tinta necessária para pinta-la, sabendo que cada litro de tinta pinta uma área de 2m²
+
