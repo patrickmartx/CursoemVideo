31f612fee0c9b3971844f249e54ad44297556eba
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-02-28 18:03:04 -0300

    Resolução do exercício 13

--- PythonExercicios/ex013.py
+++ PythonExercicios/ex013.py
@@ -1,1 +1,5 @@
 # Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+
+salarioAnterior = float(input("Qual é o salário do funcionário? R$"))
+novoSalario = salarioAnterior + (salarioAnterior * 15 / 100)
+print(f"Um funcionário que ganhava R${salarioAnterior:.2f}, com 15% de aumento, passa a receber R${novoSalario:.2f}.")
