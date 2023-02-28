dd3cfe923c66dea927b3123de592f9950346bda0
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-02-28 17:53:47 -0300

    Resolução do exercício 12

--- PythonExercicios/ex012.py
+++ PythonExercicios/ex012.py
@@ -1,1 +1,5 @@
 #Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+
+precoOriginal = float(input("Qual o preço do produto? R$"))
+precoDesconto = precoOriginal - (precoOriginal * 5 /100)
+print(f"O produto custava R${precoOriginal:.2f}, na promoção com desconto de 5% vai custar R${precoDesconto:.2f}.")
