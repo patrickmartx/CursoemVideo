825b6792b4981d4640b8ce378d7b4af17bc26de5
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-02-28 17:31:51 -0300

    Complemento exercício 8

--- PythonExercicios/ex008.py
+++ PythonExercicios/ex008.py
@@ -1,7 +1,17 @@
 # Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros.
 
 m = float(input("Digite um valor em metros: "))
+km = m / 1000
+hm = m / 100
+dam = m / 10
+dm = m * 10
 cm = m * 100
 mm = m * 1000
 
-print(f"Valor em metro: {m:.2f}\nConvertido em cm: {cm:.2f}\nConvertido em mm: {mm:.2f}")
+print(f"Valor digitado: {m}")
+print(f"Valor Quilômetros: {km}")
+print(f"Valor Hectômetros: {hm}")
+print(f"Valor Decâmetros: {dam}")
+print(f"Valor Decimetros: {dm}")
+print(f"Valor Centimetros: {cm}")
+print(f"Valor Milimetros: {mm}")
