ccebe63b3561720e55b6fd895761302f8c73b870
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-03-01 14:15:16 -0300

    Resolução do exercício 18

--- PythonExercicios/ex018.py
+++ PythonExercicios/ex018.py
@@ -1,1 +1,9 @@
 # Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
+import math
+
+angulo = float(input("Digite um ângulo: "))
+seno = math.sin(math.radians(angulo))
+cosseno = math.cos(math.radians(angulo))
+tangente = math.tan(math.radians(angulo))
+
+print(f"No ângulo {angulo}º, temos:\nSeno: {seno:.2f}\nConsseno: {cosseno:.2f}\nTangente: {tangente:.2f}")
