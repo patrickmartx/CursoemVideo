fb710e8030ab768118c1c8243ed4e3c815ff5c8d
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-03-01 15:06:01 -0300

    Resolução do exercício 21

--- PythonExercicios/ex021.py
+++ PythonExercicios/ex021.py
@@ -1,2 +1,7 @@
-# Faça um programa em python que abra e reproduza o áudio de
-# um arquivo MP3.
+# Faça um programa em python que abra e reproduza o áudio de um arquivo MP3.
+#*-coding:utf-8;-*
+from pygame import mixer
+mixer.init()
+mixer.music.load('ex021.mp3')
+mixer.music.play()
+stop = input('Digite algo para parar...')
