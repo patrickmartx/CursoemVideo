750096926eb12b965b5fcc2e4dab64e6298bbc00
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-03-01 14:24:41 -0300

    Resolução do exercício 19

--- PythonExercicios/ex019.py
+++ PythonExercicios/ex019.py
@@ -1,1 +1,10 @@
 # Um professor quer sortear um dos seus alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+from random import choice
+
+alunoA = input("Primeiro aluno(a): ")
+alunoB = input("Segundo aluno(a): ")
+alunoC = input("Terceiro aluno(a): ")
+alunoD = input("Quarto aluno(a): ")
+listaAlunos = [alunoA, alunoB, alunoC, alunoD]
+
+print(f"O aluno(a) escolhido(a) foi {choice(listaAlunos)}")
