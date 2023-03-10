dcb5c52315258746e74c4baba6bf38d66d186068
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-03-10 14:17:30 -0300

    Exercícios a serem resolvidos.

---
+++ PythonExercicios/ex043.py
@@ -0,0 +1,2 @@
+# Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu IMC e mostre seu status, de acordo com a tabela abaixo:
+# Abaixo de 18.5: Abaixo do peso | Entre 18.5 e 25: Peso ideal | 25 até 30: Sobrepeso | Acima de 40: Obesidade mórbita
