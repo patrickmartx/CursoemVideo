0c25b7835a342897e96634b7cfaae65b8b6089cc
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-03-10 14:17:10 -0300

    Conteúdo da aula 12.

--- aula12a.py
+++ aula12a.py
@@ -1,1 +1,12 @@
 #Condições Aninhadas
+
+nome = str(input("Qual o seu nome?\n")).strip().title()
+if nome == 'Patrick':
+    print("Que nome bonito!")
+elif nome == "Ana" or nome == "Beatriz" or nome == "Ana Beatriz":
+    print("Você é a mulher mais bonita do mundo!")
+elif nome == "Enzo" or nome == "Lorenzo":
+    print("KKKKKKKKKK você nasceu ontem")
+else:
+    print("Seu nome é bem normal.")
+print(f"Tenha um bom dia, {nome}!")
