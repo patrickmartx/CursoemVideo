7509df9327a07a524cf9ab885158b8b9f3a5c9c0
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-03-10 18:07:32 -0300

    Resolução do exercício 38

--- PythonExercicios/ex038.py
+++ PythonExercicios/ex038.py
@@ -1,2 +1,13 @@
 # Escreva um programa que leia dois números inteiros e compare-os, mostrando na tela uma mensagem:
 # O primeiro valor é maior | O segundo valor é maior | Não existe valor maior, os dois são iguais.
+def comparaNumero(pnum, snum):
+    if pnum > snum:
+        return print("O primeiro valor é maior.")
+    elif snum > pnum:
+        return print("O segundo valor é maior.")
+    elif pnum == snum:
+        return print("Não existe valor maior, os dois são iguais.")
+
+primeiroNumero= int(input("Digite o primeiro número: "))
+segundoNumero = int(input("Digite o segundo número: "))
+maior = comparaNumero(primeiroNumero, segundoNumero)
