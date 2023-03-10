71c4b6b3993d91d6ec3c2d44face1ec8b904384d
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-03-10 18:22:15 -0300

    Resolução do exercício 39

--- PythonExercicios/ex039.py
+++ PythonExercicios/ex039.py
@@ -1,3 +1,20 @@
-# Faça um programa que leia o ano de nascimento d eum jovem e informe de acordo com sua idade:
+# Faça um programa que leia o ano de nascimento de um jovem e informe de acordo com sua idade:
 # Se ele ainda vai se alistar ao serviço militar | Se é a hora de se alistar | Se já passou do tempo do alistamento
 # Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+from datetime import datetime
+
+def verificarAlistamento(anoNasc):
+    anoAtual = datetime.today().year
+    idade = anoAtual - anoNasc
+    if idade < 18:
+        tempoRestante = 18 - idade
+        return print(f"Você ainda irá se alistar ao serviço militar. Falta(m) {tempoRestante} ano(s).")
+    elif idade == 18:
+        return print("Já está na hora de se alistar!")
+    else:
+        tempoSuperado = idade - 18
+        return print(f"Já passou o tempo do alistamento à {tempoSuperado} ano(s).")
+
+
+anoNascimento = int(input("Em que ano você nasceu?\n> "))
+alistar = verificarAlistamento(anoNascimento)
