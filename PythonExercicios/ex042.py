87bd38bf70609a59bc7732a6d267599cb7849f7e
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-03-12 15:20:30 -0300

    Resolução do exercício 42

--- PythonExercicios/ex042.py
+++ PythonExercicios/ex042.py
@@ -1,2 +1,18 @@
 # Refaça o desafio 35 dos triângulos acrescentando o recurso de mostrar que tipo de triângulo será formado:
 # Equilátero: todos os lados iguais | Isósceles: dois lados iguais | Escaleno: Todos os lados diferentes.
+
+retaA = float(input("Digite o valor da reta A: "))
+retaB = float(input("Digite o valor da reta B: "))
+retaC = float(input("Digite o valor da reta C: "))
+
+if retaA < retaB + retaC and retaB < retaA + retaC and retaC < retaA + retaB:
+    print("É possível formar um triângulo.")
+    if (retaA == retaB) and (retaB == retaC):
+        print("É um triângulo equilátero.")
+    elif ((retaA == retaB) and (retaA != retaC)) or ((retaB == retaC) and (retaB != retaA)) or ((retaC == retaA) and retaC != retaB):
+        print("É um triângulo Isósceles.")
+    elif ((retaA != retaB) and (retaA != retaC)) or ((retaB != retaC) and (retaB != retaA)) or ((retaC != retaA) and retaC != retaB):
+        print("É um triângulo Escaleno.")
+else:
+    print("Não é possível formar um triângulo.")
+
