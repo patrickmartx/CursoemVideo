31a374a524efff2c37f39cf59d04b5ebd4649623
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-03-12 15:14:04 -0300

    Resolução do exercício 37 usando format para converter em binario, octa, e hexadecimal

--- PythonExercicios/ex037.py
+++ PythonExercicios/ex037.py
@@ -1,2 +1,23 @@
 # Escreva um programa que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão
 # 1 para binário | 2 para octal | 3 para hexadecimal
+
+def interface():
+    print("\nPROGRAMA DE CONVERSÃO\nEscolha uma opção que indique como você quer que seu numero interiro seja retornado:")
+    opcao = int(input("1 - Binário\n2 - Octal\n3 - Hexadecimal\nEscreva sua opção: "))
+    if (opcao >= 1) and (opcao <= 3):
+        return opcao
+    else:
+        print("OPÇÃO INVÁLIDA!")
+        return  interface()
+
+def conversao(nInt, op):
+    if op == 1:
+        return print(format(nInt, 'b'))
+    elif op == 2:
+        return print(format(nInt, 'o'))
+    elif op == 3:
+        return print(format(nInt, 'x'))
+
+numeroInteiro = int(input("Digite um número inteiro: "))
+opcao = interface()
+numeroConvertido = conversao(numeroInteiro, opcao)
