65fe0c020d96c9ff3302df5fa24ff40b8e9a6620
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-03-12 17:13:16 -0300

    Atualização do exercício 42.

--- PythonExercicios/ex042.py
+++ PythonExercicios/ex042.py
@@ -7,12 +7,18 @@ retaC = float(input("Digite o valor da reta C: "))
 
 if retaA < retaB + retaC and retaB < retaA + retaC and retaC < retaA + retaB:
     print("É possível formar um triângulo.")
-    if (retaA == retaB) and (retaB == retaC):
+    '''if (retaA == retaB) and (retaB == retaC):
         print("É um triângulo equilátero.")
     elif ((retaA == retaB) and (retaA != retaC)) or ((retaB == retaC) and (retaB != retaA)) or ((retaC == retaA) and retaC != retaB):
         print("É um triângulo Isósceles.")
     elif ((retaA != retaB) and (retaA != retaC)) or ((retaB != retaC) and (retaB != retaA)) or ((retaC != retaA) and retaC != retaB):
-        print("É um triângulo Escaleno.")
+        print("É um triângulo Escaleno.")'''
 else:
     print("Não é possível formar um triângulo.")
 
+if retaA == retaB == retaC:
+    print("É um triângulo equilátero.")
+elif retaA != retaB != retaC != retaA:
+    print("É um triângulo Escaleno.")
+else:
+    print("É um triângulo Isósceles.")
