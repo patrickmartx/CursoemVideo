3b5ec8317b6f18bee3c8379bb1f1b7f19da4d699
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-03-13 16:09:47 -0300

    Resolução do exercício 50.

--- PythonExercicios/ex050.py
+++ PythonExercicios/ex050.py
@@ -1,1 +1,8 @@
 # Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for impar, desconsidere-o
+
+somaPares = 0
+for i in range(0, 6):
+    numero = int(input(f"Digite o {i+1}º número: "))
+    if (numero % 2 == 0):
+        somaPares += numero
+print(somaPares)
