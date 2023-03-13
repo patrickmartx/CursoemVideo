1d5982e1a8c2db29471350a16a20212428502d1e
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-03-13 16:02:13 -0300

    Resolução do exercício 48.

--- PythonExercicios/ex048.py
+++ PythonExercicios/ex048.py
@@ -1,1 +1,7 @@
 # Faça um programa que calcule a soma entre todos os números ímpares que são múltiplos de três e que se encontram no intervalo de 1 até 500.
+
+valor = 0
+for i in range(1, 500):
+    if (i % 3 == 0):
+        valor += i
+print(valor)
