6bbb5a17ea68ef8786db754cf0e5d46f16df572f
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-03-13 16:04:31 -0300

    Resolução do exercício 49.

--- PythonExercicios/ex049.py
+++ PythonExercicios/ex049.py
@@ -1,1 +1,7 @@
 # Refaça o desafio 009 mostrando a tabuada de um número que o usuário escolher, ó que agora usando um laçõ FOR.
+
+n = int(input("Digite um número: "))
+print("="*10 + " TABUADA " + "="*10)
+for i in range(1, 11):
+    print(f"{n} X {i} = {n*i}")
+print("="*29)
