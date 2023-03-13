d6758f72c822a74908200e98a61fe08bba8b2d22
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-03-13 16:55:39 -0300

    Resolução do exercício 53 sem usar for.

--- PythonExercicios/ex053.py
+++ PythonExercicios/ex053.py
@@ -1,1 +1,15 @@
 # Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços.
+
+def checarFrase(frase):
+    frase = frase.lower().replace(" ", "")
+    fraseInversa = frase[::-1]
+    if frase == fraseInversa:
+        print("É um palíndromo!")
+    else:
+        print("Não é um palíndromo!")
+
+
+
+
+frase = str(input("Digite uma frase:\n> ")).strip()
+checarFrase(frase)
