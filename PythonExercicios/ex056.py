7f7d8bca0c3c0b8a876bb24e8fa5807069905777
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-03-13 15:57:55 -0300

    Exercícios a resolver.

---
+++ PythonExercicios/ex056.py
@@ -0,0 +1,2 @@
+# Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. No final do programa, mostre:
+# A média de idade do grupo | Qual o nome do homem mais velho | Quantas mulheres tem menos de 20 anos.
