325991668dc609ec15acf25803c8cc05b57dfeaf
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-03-14 14:12:53 -0300

    Resolução do exercício 53 usando FOR.

--- PythonExercicios/ex053.py
+++ PythonExercicios/ex053.py
@@ -2,12 +2,20 @@
 
 def checarFrase(frase):
     frase = frase.lower().replace(" ", "")
+    fraseInversa = ''
+    for letra in range(len(frase)-1, -1, -1):
+        fraseInversa += frase[letra]
+    if frase == fraseInversa:
+        return print("É palíndromo!")
+    else:
+        return print("Não é palíndromo!")
+
+'''    frase = frase.lower().replace(" ", "")
     fraseInversa = frase[::-1]
     if frase == fraseInversa:
         print("É um palíndromo!")
     else:
-        print("Não é um palíndromo!")
-
+        print("Não é um palíndromo!")'''
 
 
 
