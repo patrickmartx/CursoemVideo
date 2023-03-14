c001a8c22c8788c08c8fb126971f0f61fac7d700
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-03-14 14:04:52 -0300

    Conteúdo da aula 14.

---
+++ aula14a.py
@@ -0,0 +1,19 @@
+# Estrutura de repetição while
+
+c=1
+while c !=10:
+    print(c)
+    c+=1
+print("Acabou")
+
+n = 1
+par = impar = 0
+while n != 0:
+    n = int(input("Digite um número: "))
+    if n != 0:
+        if (n % 2 == 0):
+            par += 1
+        else:
+            impar += 1
+
+print(f"Você digitou {par} números pares e {impar} números ímpares.")
