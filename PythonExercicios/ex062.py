0791ab94c37910b2cd9a84ec4cc4f9ea521e2f4c
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-03-14 18:22:40 -0300

    Atualização do exercício 62.

--- PythonExercicios/ex062.py
+++ PythonExercicios/ex062.py
@@ -1,17 +1,15 @@
 # Melhore o desafio 61, perguntando se o usuário se ele quer mostrar mais alguns termos. O programa encerra quando ele disser que quer mostrar 0 termos.
-def tratamentoPA(p, r, limite):
-    print(f"1º termo: {p}")
-    count = 1
-    while count != limite:
-        print(f"{count+1}º termo: {p+r}")
-        p = p+r
-        count += 1
-
-
-termo = int(input("Quantos termos dessa PA você deseja ver: "))
-while termo != 0:
-    primeiroTermo = int(input("Escreva o primeiro termo da PA: "))
-    razao = int(input("Escreva a razão da PA: "))
-    tratamentoPA(primeiroTermo, razao, termo)
-    termo = int(input("Quantos termos dessa PA você deseja ver: "))
-print("FIM DO PROGRAMA")
+primeiroTermo = int(input("Escreva o primeiro termo da PA: "))
+razao = int(input("Escreva a razão da PA: "))
+termo = primeiroTermo
+cont = 1
+total = 0
+mais = 10
+while mais != 0:
+    total += mais
+    while cont <= total:
+        print(f"{cont}º Termo: {termo}")
+        termo += razao
+        cont += 1
+    mais = int(input("Quantos termos você quer mostrar a mais: "))
+print(f"Progressão finalizada com {total} termos.")
