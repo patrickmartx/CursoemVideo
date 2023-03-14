fe133a7061773b40017a50340e8563d523303b5b
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-03-14 18:37:21 -0300

    Resolução do exercício 65.

--- PythonExercicios/ex065.py
+++ PythonExercicios/ex065.py
@@ -4,15 +4,25 @@ numero = 0
 somaNumeros = 0
 count = 0
 media = 0
+maior = 0
+menor = 0
 continuar = ''
 while continuar != 'n':
     numero = float(input("Digite um número: "))
     somaNumeros += numero
     count += 1
+    if (maior == 0) and (menor == 0):
+        maior = numero
+        menor = numero
+    if numero > maior:
+        maior = numero
+    if numero < menor:
+        menor = numero
     continuar = str(input("Deseja contiuar(Responda com S ou N)?\n>")).strip().lower()
     while continuar != 'n' and continuar != 's':
         print("VALOR INVÁLIDO!")
         continuar = str(input("Deseja contiuar(Responda com S ou N)?\n>")).strip().lower()
 
 media = somaNumeros / count
-print(f"Você digitou {count} números e a média deles é de {media:.2f}.")
+print(f"Você digitou {count} números e a média deles é de {media:.2f}.\n"
+      f"O maior entre eles foi {maior} e o menor entre eles foi {menor}")
