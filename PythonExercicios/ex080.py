97bb69e10c4bcab7c649e8957a3d7f6d2c7ba234
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-03-24 20:55:08 -0300

    Exercício não resolvido.

--- PythonExercicios/ex080.py
+++ PythonExercicios/ex080.py
@@ -1,3 +1,4 @@
 # Crie um programa onde o usuário pode digitar 5 valores numéricos e cadastre-os em uma lista, já na posição correta de inserção (sem usar o sort()).
 # No final, mostre a lista ordenada na tela.
 
+
