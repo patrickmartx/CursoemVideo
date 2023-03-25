7ff4e91ae9e67ae4d0ae7ac3e9258214840ebc41
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-03-25 15:47:18 -0300

    Resolução do exercício 82.

--- PythonExercicios/ex082.py
+++ PythonExercicios/ex082.py
@@ -1,3 +1,24 @@
 # Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores ímpares digitados, respectivamente. Ao final, mostre o conteúdo das três listas geradas.
 
+lista = []
+pares = []
+impares = []
 
+while True:
+    val = int(input("Digite um número: "))
+    lista.append(val)
+    confirma = str(input("Quer continuar? [S/N] "))
+    while confirma not in 'Ss' and confirma not in 'Nn':
+        confirma = str(input("VALOR INVÁLIDO!\nQuer continuar? [S/N] "))
+    if confirma in 'Nn':
+        break
+
+for i in range(0, len(lista)):
+    if lista[i] % 2 == 0:
+        pares.append(lista[i])
+    else:
+        impares.append(lista[i])
+
+print(lista)
+print(pares)
+print(impares)
