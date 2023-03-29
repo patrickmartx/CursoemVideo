b4bbedd1e44f580960cffef5d987163f46f12531
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-03-29 15:51:36 -0300

    Resolução do exercício 97.

--- PythonExercicios/ex097.py
+++ PythonExercicios/ex097.py
@@ -1,9 +1,9 @@
 # Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parãmetro e mostre uma mensagem com tamanho adaptável.
 def escrevaTexto(txt):
-    tam = len(txt)
-    print("~" * (tam + 4))
-    print(f"{txt:^{(tam + 4)}}")
-    print("~" * (tam + 4))
+    tam = len(txt) + 4
+    print("~" * (tam))
+    print(f"{txt:^{(tam)}}")
+    print("~" * (tam))
 
 
 escrevaTexto("Patrick Martins")
