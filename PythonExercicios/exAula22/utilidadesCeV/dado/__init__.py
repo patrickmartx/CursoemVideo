c8e63d94a310cc600193d32d314a8891ac10ef8d
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-03-31 17:37:47 -0300

    alteração no módulo dado.

--- PythonExercicios/exAula22/utilidadesCeV/dado/__init__.py
+++ PythonExercicios/exAula22/utilidadesCeV/dado/__init__.py
@@ -1,7 +1,7 @@
 def leiaDinheiro(txt):
-    valor = input(txt).replace(',', '.')
+    valor = input(txt).replace(',', '.').strip()
     while valor.isalpha() or valor.count('.') > 1 or valor.strip() == '':
         print(f"\033[0;31mVALOR INVÁLIDO! \"{valor}\" não é aceito. Insira um valor numérico.\033[m")
-        valor = input(txt).replace(',', '.')
+        valor = input(txt).replace(',', '.').strip()
     return float(valor)
 
