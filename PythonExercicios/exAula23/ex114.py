2a0b70c5c9e3931cd2d32ed8a1ef81f8dd45c3b0
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-03-31 20:11:15 -0300

    Resolução do exercício 114.

---
+++ PythonExercicios/exAula23/ex114.py
@@ -0,0 +1,12 @@
+# Crie um código em Python que teste se o site Pudim está acessível pelo computador usado.
+import urllib.request
+from utils.data import imprimir
+
+try:
+    site = urllib.request.urlopen('http://pudim.com.br/')
+except urllib.error.URLError:
+    imprimir('Não é possível abrir este site. Cheque sua conexão ou a existência do mesmo.', 1)
+except Exception as erro:
+    imprimir(f'O site não está acessível no momento, erro {erro.__class__}', 3)
+else:
+    imprimir('Consegui abrir o site com sucesso!', 2)
